d2d5248fa5c0dbdbf6173589b7cb5994f9bef1db
Flame_Bannawit <flamebannawit@gmail.com>
2026-05-17 04:13:42 +0700

    fix: add redirect from / to /dashboard/

--- dashboard_server.py
+++ dashboard_server.py
@@ -1,6 +1,12 @@
 import os
 from dashboard.app import app
+from flask import redirect
 
 server = app.server
+
+@server.route("/")
+def index():
+    return redirect("/dashboard/")
+
 port = int(os.environ.get("PORT", 8050))
 app.run(host="0.0.0.0", port=port, debug=False)
